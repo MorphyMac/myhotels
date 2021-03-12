838abc1788384c198dc9e7709a2de85b1683bce8
Anwar_Alam <Chikoo@jan10>
2021-03-12 12:59:34 +0530

    added hystrix-dashboard

---
+++ hystrix-dashboard/src/test/java/com/epam/myhotels/hysrtixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.myhotels.hysrtixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
