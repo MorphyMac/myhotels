c602c6c06862c5ecdc39268f2f627db9978a3ef1
Anwar_Alam <Chikoo@jan10>
2021-03-12 16:55:21 +0530

    added swagger support to hotels-service

--- hotels-service/src/main/java/com/epam/myhotels/hotels/configs/SecurityConfiguration.java
+++ hotels-service/src/main/java/com/epam/myhotels/hotels/configs/SecurityConfiguration.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +24,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/**").permitAll();
         http.headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        String whitelistUrlsStr = environment.getProperty("app.security.whitelisted.urls");
+        assert whitelistUrlsStr != null;
+        String[] whitelistUrls = whitelistUrlsStr.split(",");
+
+        web.ignoring().antMatchers(Arrays.stream(whitelistUrls).filter(str -> !str.isBlank()).map(String::trim)
+                                         .toArray(String[]::new));
     }
 }
